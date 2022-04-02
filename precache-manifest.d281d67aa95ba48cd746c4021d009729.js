815d2a80fc9b3077329ef0ac0e9339fbe9f6792a
shiyasmohd <shiyasjaseena124@gmail.com>
2022-04-03 03:34:14 +0530

    Updates

---
+++ precache-manifest.d281d67aa95ba48cd746c4021d009729.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb377189c2925d4abeb0",
+    "url": "/shieldx/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "bb377189c2925d4abeb0",
+    "url": "/shieldx/static/js/main.e225c72a.chunk.js"
+  },
+  {
+    "revision": "793361fe1b4834af8712",
+    "url": "/shieldx/static/js/runtime~main.81ab09bc.js"
+  },
+  {
+    "revision": "fecc44f2447f2ea7c903",
+    "url": "/shieldx/static/css/2.a03f6f6b.chunk.css"
+  },
+  {
+    "revision": "fecc44f2447f2ea7c903",
+    "url": "/shieldx/static/js/2.e65e1b38.chunk.js"
+  },
+  {
+    "revision": "6025bf8e1c7a7268fe822cf6f9a115bc",
+    "url": "/shieldx/index.html"
+  }
+];
